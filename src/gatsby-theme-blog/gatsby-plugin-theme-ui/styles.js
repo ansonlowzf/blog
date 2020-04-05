70201a64fd4c0ba7391a177e700585da2e43ee1e
Anson Low Z.F <zhanfaye@gmail.com>
2020-04-05 18:15:37 +0800

    styles : markdown img

---
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/styles.js
@@ -0,0 +1,8 @@
+import defaultThemeStyles from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/styles"
+
+export default {
+  ...defaultThemeStyles,
+  img: {
+    boxShadow: `0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`,
+  },
+}
