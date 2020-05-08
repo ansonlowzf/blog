c668d473ed8fc1044a103ba3d28a90eb151c99ce
Anson Low Z.F <zhanfaye@gmail.com>
2020-05-08 15:38:59 +0800

    add link styling

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/styles.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/styles.js
@@ -5,4 +5,12 @@ export default {
   img: {
     boxShadow: `0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`,
   },
+  a: {
+    color: `primary`,
+    textDecoration: `none`,
+
+    ":hover, :focus": {
+      color: `orange`,
+    },
+  },
 }
