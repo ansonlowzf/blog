33c2a7f0cb8c8cdffda8d3289576646c3d2b5cf4
Anson LowZF <zhanfaye@gmail.com>
2020-05-28 16:05:14 +0800

    unwanted

---
+++ src/my-components/MyTwitter.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter } from "@fortawesome/free-solid-svg-icons"
+
+const MyTwitter = () => {
+  return (
+    <a href="https://twitter.com/ansonlowzf">
+      <FontAwesomeIcon icon={faTwitter} />
+    </a>
+  )
+}
+
+export default MyTwitter
